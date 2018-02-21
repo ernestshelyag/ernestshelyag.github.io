0eede1401dea3ad1029731d74595a45fa1d9b337
Эрик <ernest.shelyag@gmail.com>
2018-02-21 23:07:22 +0300

    new commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$('.exp-list__btn').on('click', function(){
+	$('.exp-list__btn').toggleClass('active');
+	$('.exp-list__content').toggleClass('active');
+});
+
+
+
+
+
+
+
---
+++ blocks/main.js
@@ -0,0 +1,4 @@
+$('.exp-list__btn').on('click', function(){
+	$('.exp-list__btn').toggleClass('active');
+	$('.exp-list__content').toggleClass('active');
+});
